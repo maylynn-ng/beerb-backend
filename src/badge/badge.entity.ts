bc6eae2aa90666d181f040fae94ed0ac580fdee6
Maylynn <maylynn.ng94@gmail.com>
2021-02-12 19:57:02 +0000

    CHORE: create badge entity

---
+++ src/badge/badge.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class Badge {
+  @Column({ type: 'varchar', length: 300, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 300, nullable: false })
+  text: string;
+
+  @Column({ type: 'varchar', length: 300, nullable: false })
+  image: string;
+}
