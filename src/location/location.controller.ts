55a9b93ab26a89d5375c9108297dee86f481ee0b
Maylynn <maylynn.ng94@gmail.com>
2021-02-15 17:39:57 +0000

    FEAT: link location controllers to service

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { LocationService } from './location.service';
+import { Location } from './location.entity';
 
 @Controller('location')
 export class LocationController {
+  constructor(private readonly locationService: LocationService) {}
+
   @Post('location')
   postLocation(
     @Body() body: { UserId: number; location: {}; beer: number },
@@ -10,7 +14,7 @@ export class LocationController {
   }
 
   @Post('locations')
-  getLocations(@Body() body: { sub: string }): string {
-    return 'find user info or create user';
+  getLocations(@Body() body: { sub: string }): Promise<Location[]> {
+    return this.locationService.getLocations(body);
   }
 }
